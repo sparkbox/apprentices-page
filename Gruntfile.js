25b8c4692df4b39230fed25e2917f8b88311662b
Div <divya@heysparkbox.com>
2014-10-01 16:44:32 -0400

    fix: Changing gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   });
   grunt.loadTasks("grunt");
   grunt.registerTask("compile", ["sass", "copy"])
-  grunt.registerTask("dev", ["compile", "copy", "connect", "watch"])
+  grunt.registerTask("dev", ["compile", "connect", "watch"])
   grunt.registerTask("buildProduction", ["compile", "validation"]);
   // Default task
   return grunt.registerTask("default", "dev")
