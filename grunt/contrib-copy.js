62baf8f0e8453e75ef455b2484f58d157e89a17a
Div <divya@heysparkbox.com>
2014-10-08 10:00:02 -0400

    fix: Updating package.json to include grunticon

--- grunt/contrib-copy.js
+++ grunt/contrib-copy.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
   grunt.config("copy", {
     main: {
-      expand: true,
-      cwd: "pages/",
-      src: "*.html",
-      dest: "dist/"
+      files: [
+        {expand: true, cwd: "pages/", src: "*.html", dest: "dist/"},
+        {expand: true, cwd: "img/", src: "*", dest: "dist/img"}
+      ]
     }
   });
   return grunt.loadNpmTasks('grunt-contrib-copy');
