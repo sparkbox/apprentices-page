398b03b6fd713f86165ce136da2062d0fdf6574e
Div <divya@heysparkbox.com>
2014-10-21 15:28:04 -0400

    fix: Adding no-mq for ie

--- grunt/contrib-sass.js
+++ grunt/contrib-sass.js
@@ -2,7 +2,8 @@ module.exports = function(grunt) {
   grunt.config("sass", {
     dist: {
       files: {
-        'dist/css/mqs.css' : 'scss/mqs.scss'
+        'dist/css/mqs.css' : 'scss/mqs.scss',
+        'dist/css/no-mqs.css' : 'scss/no-mqs.scss'
       }
     }
   });
