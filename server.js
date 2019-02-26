6fe2890da26200369c62d82f99e6e973a929ad84
Anastasia Lanz <anastasia@heysparkbox.com>
2019-02-26 11:15:25 -0500

    fix: move express.static after enforcing SSL

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('dist', { extensions: ['html'] }));
-
 if (process.env.ENFORCE_SSL) {
   app.enable('trust proxy');
   app.use(enforceSSL());
 }
+
+app.use(express.static('dist', { extensions: ['html'] }));
 
 var server = app.listen(app.get('port'), () => {
   var host = server.address().address;
