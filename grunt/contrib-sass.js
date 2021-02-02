f6d324cc3a12e7053161a30ef8b04c256db2e690
Adam Simpson <adam@heysparkbox.com>
2021-02-02 15:28:46 -0500

    chore: migrate to dart sass

--- grunt/contrib-sass.js
+++ grunt/contrib-sass.js
@@ -1,11 +1,16 @@
+var sass = require('sass');
+
 module.exports = function(grunt) {
-  grunt.config("sass", {
+  grunt.config('sass', {
+    options: {
+      implementation: sass,
+    },
     dist: {
       files: {
-        'dist/css/mqs.css' : 'scss/mqs.scss',
-        'dist/css/no-mqs.css' : 'scss/no-mqs.scss'
-      }
-    }
+        'dist/css/mqs.css': 'scss/mqs.scss',
+        'dist/css/no-mqs.css': 'scss/no-mqs.scss',
+      },
+    },
   });
-  return grunt.loadNpmTasks('grunt-contrib-sass');
+  return grunt.loadNpmTasks('grunt-sass');
 };
